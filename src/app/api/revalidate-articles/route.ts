13f29fe56c202914d105cf86836fc8df86f77cb8
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2024-08-01 15:44:10 +0300

    Update route.ts

--- src/app/api/revalidate-articles/route.ts
+++ src/app/api/revalidate-articles/route.ts
@@ -3,10 +3,12 @@ import { revalidateTag } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 // eslint-disable-next-line import/prefer-default-export
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
-    await getRssFeed();
-    revalidateTag('update-articles');
+    if (request.headers.get('secret') === process.env.SECRET) {
+      await getRssFeed();
+      revalidateTag('update-articles');
+    }
     return NextResponse.json({ message: 'OK' }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
