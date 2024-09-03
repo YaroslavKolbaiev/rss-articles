57d8a474f6fdb76dc24e58dc0eba75afdb4b39a2
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2024-09-03 18:30:36 +0300

    Update ApproveArticle.tsx

--- src/components/Article/ApproveArticle.tsx
+++ src/components/Article/ApproveArticle.tsx
@@ -13,7 +13,7 @@ export default function ApproveArticle({ approved, id }: Props) {
       {approved ? (
         <span className="inline-flex items-center justify-center gap-1">
           <span className="text-sm text-[#028A0F]">approved</span>
-          <Image src="TickIcon.svg" alt="Approved" width={14} height={14} />
+          <Image src="TickIcon.svg" alt="✓" width={14} height={14} />
         </span>
       ) : (
         <form action={approveArticle} className="inline-flex">
